cfc47d4cfb284c4b079d27ecd91823b0a5a2758f
Bale <bale7@qq.com>
2018-05-10 22:44:01 +0800

    add .

---
+++ element-demo/src/components/Date.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <el-date-picker
+      v-model="value1"
+      type="date"
+      placeholder="选择日期"
+      :picker-options="pickerOptions0">
+    </el-date-picker>
+	<div>{{value1}}</div>
+</div>
+</template>
+<script>
+	export default{
+		data(){
+			return {
+				value1:'',
+				pickerOptions0:''
+			}
+		}
+	}
+</script>
